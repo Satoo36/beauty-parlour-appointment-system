5c2fa27922cf5e73b79275a781e1b40180daade9
kavya <kavyanerella65@gmail.com>
2026-04-27 16:04:28 +0530

    Fix Render crash - remove frontend serving

--- server/server.js
+++ server/server.js
@@ -101,12 +101,9 @@ app.use('/api/payment', paymentRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Static file serving for production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
+    app.get('/', (req, res) => {
+        res.send("API is running");
     });
 }
 
